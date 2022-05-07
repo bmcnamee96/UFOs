97da0ba3d1df419fdd36a08fe20903e2cee7175e
bmcnamee96 <bmac965@gmail.com>
2022-05-07 14:06:58 -0400

    created a function ability to the table

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,29 @@ function buildTable(data){
         });
     });
 }
+
+function handleClick() {
+    // select the first id in the HTML tag and store it in the date variable
+    let date = d3.select("#datetime").property("value");
+    
+    // allow users to filter the data
+    // tableData is all of our data in the js file
+    let filteredData = tableData;
+    
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+    buildTable(filteredData);
+}
+
+// run the handleClick function on the button click
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
